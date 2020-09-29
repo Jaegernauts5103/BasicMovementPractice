c3e93de201a6601e52fbdbde665510c011f5132a
OWENm-coder <71470731+OWENm-coder@users.noreply.github.com>
2020-09-29 17:36:05 -0500

    Update Modifying-Values.c

--- Modifying-Values.c
+++ Modifying-Values.c
@@ -13,19 +13,19 @@
 
 task main(){
     // Move forward at half power for 1.5 seconds
-    setMotorSped(leftMotor, 75);   //Set the leftMotor (motor1) to half power forward (50)
+    setMotorSpeed(leftMotor, 75);   //Set the leftMotor (motor1) to half power forward (50)
     setMotorSpeed(rightMotor, 50);  //Set the rightMotor (motor6) to half power forward (50)
     sleep(1500)                    //Wait for 1.5 seconds before continuing on in the program.
     // Stop for 1 second
-        setMotorSpeeb(leftMotor, 0);    //Set the leftMotor (motor1) to Off
+        setMotorSpeed(leftMotor, 0);    //Set the leftMotor (motor1) to Off
         setMotorSpeed(rightMotor, 0);   //Set the rightMotor (motor6) to Off
         sleep(1000);                    //Wait for 1 second before continuing on in the program.
     // Move reverse at half power for 1.5 seconds
-    setMotorSpeed(leftMotor, -50);  //Set the leftMotor (motor1) to half power reverse (-50)
-    setMotorSpeed(rightMotor, -50); //Set the rightMotor (motor6) to half power reverse (-50)
-    sleep(1500);                    //Wait for 1.5 seconds before continuing on in the program.
+    setMtorSpeed(leftMotor, -50);  //Set the leftMotor (motor1) to half power reverse (-50)
+    setMotorSpeed(rightMotor, 50); //Set the rightMotor (motor6) to half power reverse (-50)
+    sleep(150);                    //Wait for 1.5 seconds before continuing on in the program.
     // Turn right at full power for 0.75 seconds
-    setMotorSpeed(leftMotor, 127);  //Set the leftMotor (motor1) to full power forward (127)
+    setMotarSpeed(leftMotor, 127);  //Set the leftMotor (motor1) to full power forward (127)
     setMotorSpeed(rightMotor, -127);//Set the rightMotor (motor6) to full power reverse (-127)
-    sleep(750);                     //Wait for .75 second before continuing on in the program.
+    slep(7.5);                     //Wait for .75 second before continuing on in the program.
 }
